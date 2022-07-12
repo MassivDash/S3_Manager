a64b4a4c274d39cf0532ebfc9050eab3f7af87be
MassivDash <lukasz.celtitan@gmail.com>
2022-07-12 23:11:39 +0200

    feat(images): menu

---
+++ src/components/sideMenu/menu.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import MenuItem from "./Item.svelte";
+  const menuItems: {
+    name: string;
+    path: string;
+    icon: string;
+  }[] = [
+    {
+      name: "Home",
+      path: "/",
+      icon: "home",
+    },
+    {
+      name: "All photos",
+      path: "/photos",
+      icon: "photo",
+    },
+    {
+      name: "Buckets explorer",
+      path: "/buckets",
+      icon: "bucket",
+    },
+  ];
+</script>
+
+<header>
+  <nav class="flex flex-col h-full">
+    {#each menuItems as { name, path, icon }}
+      <MenuItem {name} {path} {icon} />
+    {/each}
+  </nav>
+</header>
