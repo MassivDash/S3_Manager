dbc9a04214f1b7d40e419c5ba05ee19f305de2f4
SpaceGhost <lukasz.celitan@gmail.com>
2022-07-19 21:40:41 +0200

    feat(transitions): add page transitions

---
+++ src/components/fadeIn/fadeInRoute.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import { fade } from "svelte/transition";
+  import { Route } from "svelte-navigator";
+  export let path;
+</script>
+
+<Route {path}>
+  <div in:fade>
+    <slot />
+  </div>
+</Route>
