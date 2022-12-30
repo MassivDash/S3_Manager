b3e45a7ba085f3bcb806b24bad0c7e1a45cec903
SpaceGhost <lukasz.celitan@gmail.com>
2022-12-30 17:00:05 +0100

    feat(pages):  add save user options in writable

--- src/store/images.ts
+++ src/store/images.ts
@@ -3,3 +3,6 @@ import { writable } from 'svelte/store';
 
 
 export const images = writable(null);
+export const images_scroll_index = writable(null);
+export const image_grid_option = writable(null); 
+
