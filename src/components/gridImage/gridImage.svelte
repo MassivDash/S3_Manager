91b9df2609a00b560649d161c38f4c5b2c73bb75
SpaceGhost <lukasz.celitan@gmail.com>
2022-12-31 14:44:06 +0100

    feat(img): add error handeling on grid images

--- src/components/gridImage/gridImage.svelte
+++ src/components/gridImage/gridImage.svelte
@@ -3,6 +3,7 @@
   import { useLazyImage as lazyImage } from "svelte-lazy-image";
   import Checkbox from "../checkbox/checkbox.svelte";
   import { formatBytes } from "../../lib/date";
+  import Error from "../icons/error.svelte";
 
   import Tags from "../tags/tags.svelte";
   import type { ImageBucket, CheckedFile } from "src/types";
@@ -15,6 +16,7 @@
   export let name: string;
   export let checkedFiles: CheckedFile[];
   export let handleCheckbox: (key: string, bucketName: string) => void;
+  export let imgError = false;
 
   // const tags = aws_tags.map(tag => tag.value ? `${tag.key}-${tag.value}` :  tag.key)
 
@@ -49,12 +51,27 @@
   </div>
   <Link to="{bucket.name}/{key}" class="flex flex-col justify-center">
     <div>
-      <img
-        data-src={url}
-        alt={key}
-        class="rounded-sm w-full object-cover min-h-[500px]"
-        use:lazyImage
-      />
+      {#if !imgError}
+        <img
+          data-src={url}
+          alt={key}
+          class="rounded-sm w-full object-cover min-h-[500px]"
+          use:lazyImage
+          on:error={() => {
+            imgError = true;
+          }}
+        />
+      {/if}
+      {#if imgError}
+        <div class="p-4 mt-4 flex gap-4 h-full w-full  text-red-700">
+          <div><Error width={36} height={36} /></div>
+          <div class="text-sm">
+            <h3>Image not loaded</h3>
+            <p>Most probably the presigned url has expired.</p>
+            <p>Try to resync</p>
+          </div>
+        </div>
+      {/if}
     </div>
   </Link>
   <div class="absolute bottom-0 opacity-70 w-full">
