437cc9ac3629035f504d84c2e75061ee0c75c0d2
MassivDash <lukasz.celtitan@gmail.com>
2023-03-13 10:53:19 +0100

    feat(aws_client): expose non-standart url endpoint via system env

--- src-tauri/src/lib/s3/client/client.rs
+++ src-tauri/src/lib/s3/client/client.rs
@@ -5,7 +5,22 @@ use aws_sdk_s3::Client;
 
 pub async fn create_client() -> Result<Client, Box<dyn Error + Send + Sync>> {
     let region_provider = RegionProviderChain::default_provider().or_else("us-east-1");
-    let config = aws_config::from_env().region(region_provider).load().await;
+    let mut config_builder = aws_config::from_env().region(region_provider);
+
+    if use_custom_endpoint() {
+        let endpoint = std::env::var("S3_CUSTOM_ENDPOINT_URL");
+        match endpoint {
+            Ok(endpoint) => {
+                config_builder = config_builder.endpoint_url(endpoint);
+            }
+            Err(err) => return Err(err.into()),
+        }
+    };
+    let config = config_builder.load().await;
     let client = Client::new(&config);
     Ok(client)
+}
+
+fn use_custom_endpoint() -> bool {
+    std::env::var("S3_CUSTOM_ENDPOINT").is_ok()
 }
