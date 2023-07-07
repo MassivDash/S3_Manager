5d189b8e2c63339f9f7951c2731de6c2a67930c7
SpaceGhost <lukasz.celitan@gmail.com>
2023-07-07 15:01:32 +0200

    test(suites): fix config, add setup, add tests

---
+++ src/components/circularProgress/circularProgress.test.ts
@@ -0,0 +1,22 @@
+import { render } from '@testing-library/svelte';
+import CircularProgress from './circularProgress.svelte';
+import { describe, it } from 'vitest'
+import '@testing-library/jest-dom'
+
+
+describe('CircularProgress', () => {
+  it('should render with 0% progress', () => {
+    const { getByText } = render(CircularProgress, { progress: 0 });
+    expect(getByText('0%')).toBeInTheDocument();
+  });
+
+  it('should render with 50% progress', () => {
+    const { getByText } = render(CircularProgress, { progress: 0.5 });
+    expect(getByText('50%')).toBeInTheDocument();
+  });
+
+  it('should render with 100% progress', () => {
+    const { getByText } = render(CircularProgress, { progress: 1 });
+    expect(getByText('100%')).toBeInTheDocument();
+  });
+});
