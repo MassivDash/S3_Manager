5f25813ed915b8408cae8764e72247ca0f0dfc0e
SpaceGhost <lukasz.celitan@gmail.com>
2023-07-14 14:16:42 +0200

    feat(error): add better error front side handling for known cases

--- src/components/modal/content.svelte
+++ src/components/modal/content.svelte
@@ -1,8 +1,8 @@
 <script>
   import Error from "../icons/error.svelte";
 
-  export let message = "Hi";
-  export let title = "Modal";
+  export let message = "Critical";
+  export let title = "Unknown error";
   export let type = "info";
 </script>
 
@@ -15,18 +15,64 @@
   <div>
     <h2 class="text-lg">{title}</h2>
     <p class="text-sm text-red-700">
-      response error: <span class="text-base text-black">{message}</span>
+      error details: <span class="text-base text-black">{message}</span>
     </p>
   </div>
 </div>
-<ul class="mt-8 text-xs">
-  <li class="m-2">
-    - Check if your aws config files "config" and "credentials" are present on
-    your current machine, for more info:
-    https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-files.html
-  </li>
-  <li class="m-2">
-    - Aws permissions are set correctly for your S3 access read and write
-    operations in AWS IAM console. (Non root user)
-  </li>
-</ul>
+{#if message === "failed to construct request"}
+  <div class="mt-8">
+    <p>
+      Failed to construct request usually refers to missing or incorrect
+      credentials
+    </p>
+    <ul class="mt-2 text-xs">
+      <li class="m-2">
+        - Check if your aws config files "config" and "credentials" are present
+        on your current machine, for more info:
+        https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-files.html
+      </li>
+      <li class="m-2">
+        - Aws permissions are set correctly for your S3 access read and write
+        operations in AWS IAM console. (Non root user)
+      </li>
+    </ul>
+  </div>
+{/if}
+
+{#if message === "service error"}
+  <div class="mt-8">
+    <p>
+      Service error usually refers to incorrect credentials, either access key
+      or access id is incorrect
+    </p>
+    <ul class="mt-2 text-xs">
+      <li class="m-2">
+        - Check if your aws config file "credentials" contains valid connection
+        keys
+      </li>
+    </ul>
+  </div>
+{/if}
+
+{#if message === "dispatch failure"}
+  <div class="mt-8">
+    <p>Unable to connect</p>
+    <ul class="mt-2 text-xs">
+      <li class="m-2">- Check your internet connection</li>
+    </ul>
+  </div>
+{/if}
+
+{#if !["service error", "failed to construct request", "dispatch failure"].includes(message)}
+  <div class="mt-8">
+    <p>S3 unknown error</p>
+    <ul class="mt-2 text-xs">
+      <li class="m-2">
+        - Please report the issue on <a
+          href="https://github.com/MassivDash/S3_Manager/issues"
+          target="_blank">https://github.com/MassivDash/S3_Manager/issues</a
+        >
+      </li>
+    </ul>
+  </div>
+{/if}
